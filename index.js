a900e6f5042b93c5454d6d25f9060a32f3e07386
rutvik2699 <57801263+Rutvik2699@users.noreply.github.com>
2022-08-27 02:17:44 +0000

    Array Methods

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-console.log("This replit is about Variables in JS")
-let b= "Harry";
-let c = null;
-let d = undefined;
-console.log(b);
-{
-  let b = 'this'
-  console.log(b)
-}
+//Array methods
+let num = [1,2,3,34,5]
+console.log(num)
+let b = num.toString()//b is now a string
+console.log(b,typeof b)
+let c = num.join("_")
+console.log(c,typeof c)
+
+let r = num.pop() // pop returns the popped element\
+console.log(num,r)
+let r = num.push(56) //push returns the new array length
+console.log(num,r)
+
+let r = num.shift() //re3moves an element from start of the array
+console.log(r,num)
+
+let r =num.unshift(78) //
+console.log(r,num)
