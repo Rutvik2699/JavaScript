1e8eeb9e24c0a4545ac19a66fe81921e5d1342ca
rutvik2699 <>
2022-08-27 15:27:15 +0000

    First commit for array methods 2

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-console.log("This replit is about Variables in JS")
-let b= "Harry";
-let c = null;
-let d = undefined;
-console.log(b);
-{
-  let b = 'this'
-  console.log(b)
+ // let num = [1,2,3,4,5,6,7,8,9]
+// console.log(num.length)
+// delete num[0] //Deleting item from does not reduce size of array
+// console.log(num)
+// console.log(num.length)
+
+//  let num_more = [11,12,13,14,15,16,17,18,19]
+// let newArray1 = [11,12,13,14,15,16,17,18,19]
+// let newArray = num.concat(num_more)
+// console.log(newArray)
+
+//Sort method
+
+let compare = (a,b)=>{
+  return b-a
 }
+let num = [551,666,872,700]
+//Sort the array
+num.sort(compare)
+console.log(num)
+
+//Reverse the array
+num.reverse()
+console.log(num)
+
+
+//Splice and Slice
